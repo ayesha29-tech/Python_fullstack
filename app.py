9e39f68433922249f2a7a3520a79cc7b72808ca5
Ayesha <itzursaishhtakia@gmail.com>
2026-08-05 09:06:23 +0000

    Auto sync updates

--- app.py
+++ app.py
@@ -37,5 +37,25 @@ def register():
         course=request.form["course"]
         return render_template("register.html")
     return render_template("register.html")
+
+@app.route('/login',methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        return render_template("login.html")
+    return render_template("login.html")
+
+
+@app.route('/api/register',methods=["POST"])
+def api_register():
+    data = request.get_json()
+    email = data.get("email")
+
+    if email in users_db:
+        return jsonify({"status": "error" , "message" : "User already exists with this email!"}),400
+
+    #Save user to our simple database
+    users_db[email]=data
+    return jsonify({"status": "success", "message":"Registration successful!"})
+
 if __name__ == '__main__':
     app.run(debug=True)
